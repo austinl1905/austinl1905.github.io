70c0a2d720f09d62eec6412eb5cd5babd619a7fe
austinl1905 <114597547+austinl1905@users.noreply.github.com>
2022-09-28 10:55:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Austin Liu"; // update the part BETWEEN the "quotes"
+let userName = "austinl1905"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
